948ce094c3e36faec4f26bd19439a0effa279779
Marcus Schreiter <marcus.schreiter@meso.design>
2022-02-03 21:32:14 +0100

    add short notice about installation offline usage

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,17 +3,20 @@
 </script>
 
 <Subpage>
-	<div>
+	<p>
+		This metronome is offline capable and installable. You can add it to your homescreen
+		on mobile or install it on a desktop computer and use it like a native app. For more
+		information see <a
+			href="https://mobilesyrup.com/2020/05/24/how-install-progressive-web-app-pwa-android-ios-pc-mac/"
+			target="_blank">this guide</a
+		>.
+	</p>
+	<p>—————————————</p>
+	<p>
 		by <a href="https://github.com/marcusand" target="__blank">marcusand</a>
-	</div>
-	<div>
+	</p>
+	<p>
 		report any issues on <a href="https://github.com/marcusand/metronome">github</a>
 		or send me an <a href="mailto:mail@marcusand.de">email</a>
-	</div>
+	</p>
 </Subpage>
-
-<style>
-	div {
-		margin: 0.5rem 0;
-	}
-</style>
