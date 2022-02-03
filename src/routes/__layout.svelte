49b7ae7708f986929e5a0d6f3fc1daff21cab52c
Marcus Schreiter <marcus.schreiter@meso.design>
2022-02-03 21:07:38 +0100

    update and move site manifest

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,12 +9,12 @@
 	<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
 	<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
 	<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-	<link rel="manifest" href="/icons/site.webmanifest" />
+	<link rel="manifest" href="/site.webmanifest" />
 	<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
 	<link rel="shortcut icon" href="/icons/favicon.ico" />
 	<meta name="msapplication-TileColor" content="#b91d47" />
 	<meta name="msapplication-config" content="/icons/browserconfig.xml" />
-	<meta name="theme-color" content="#ffffff" />
+	<meta name="theme-color" content="#491280" />
 </svelte:head>
 
 <div class="outer-container">
